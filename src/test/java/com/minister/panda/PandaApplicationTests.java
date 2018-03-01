914124cf7aac08d885f3cd7d25431b8f97778c1e
Liqing <liq@live.cn>
2018-03-02 00:31:41 +0800

    测试panda第二次提交

--- src/test/java/com/minister/panda/PandaApplicationTests.java
+++ src/test/java/com/minister/panda/PandaApplicationTests.java
@@ -1,16 +1,85 @@
 package com.minister.panda;
 
+import com.minister.panda.web.UserController;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PandaApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	private MockMvc mvc;
+
+	@Before  //这个方法在每个方法执行之前都会执行一遍
+	public void setUp() throws Exception {
+		mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
 	}
 
+	@Test
+	public void testUserController() throws Exception {
+		//test UserController
+		RequestBuilder request = null;
+		// 1、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 2、post提交一个user
+		request = post("/users/")
+				.param("id", "1")
+				.param("name", "测试大师")
+				.param("age", "20");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 3、get获取user列表，应该有刚才插入的数据
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[{\"id\":1,\"name\":\"测试大师\",\"age\":20}]")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 4、put修改id为1的user
+		request = put("/users/1")
+				.param("name", "测试终极大师")
+				.param("age", "30");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 5、get一个id为1的user
+		request = get("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("{\"id\":1,\"name\":\"测试终极大师\",\"age\":30}")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 6、del删除id为1的user
+		request = delete("/users/1");
+		mvc.perform(request)
+				.andExpect(content().string(equalTo("success")))
+				.andDo(MockMvcResultHandlers.print());
+
+		// 7、get查一下user列表，应该为空
+		request = get("/users/");
+		mvc.perform(request)
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[]")))
+				.andDo(MockMvcResultHandlers.print());
+
+	}
 }
